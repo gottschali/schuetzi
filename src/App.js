2e3fdfac9d5719e9692c27122530570fad83f3c7
Gottschali <aligottschall@protonmail.com>
2022-02-12 16:17:40 +0100

    Make conmpass working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+import ReactCompass from './ReactCompass';
 
 function App() {
+  const [rangeVal, setRangeVal] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactCompass direction={rangeVal}/>
+      <input type="range" max="360" min="0"
+          onChange={(e) => setRangeVal(parseInt(e.target.value, 10))} onInput={(e) => setRangeVal(parseInt(e.target.value, 10))}/>
     </div>
   );
 }
