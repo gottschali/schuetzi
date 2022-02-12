1a8f571debf3c994c1055133a462ce691e3f8ff9
Gottschali <aligottschall@protonmail.com>
2022-02-12 18:35:53 +0100

    Two sliders

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import { useState, useEffect } from 'react';
 
 import ReactCompass from './ReactCompass';
 
-function heading(c1, c2) {
-  const la1 = c1.latitude;
-  const lo1 = c1.longitude;
-  const la2 = c2.latitude;
-  const lo2 = c2.longitude;
-  return Math.atan2(Math.cos(la1) * Math.sin(la2) - Math.sin(la2) * Math.cos(la2) * Math.cos(lo2 - lo1)
-                    , Math.sin(lo2 - lo1) * Math.cos(la2));
-}
 function bearing(c1, c2) {
   /*
    * Δφ = ln( tan( latB / 2 + π / 4 ) / tan( latA / 2 + π / 4) )
@@ -42,28 +34,38 @@ const schutziLong = 8.71749;
 const Schutzi = new Coordinate(schutziLat, schutziLong);
 
 function App() {
-  const [rangeVal, setRangeVal] = useState(0);
   const [coords, setCoords] = useState();
+  const [heading, setHeading] = useState(0); // heading north to the compass
+  const [schutzDirection, setSchutzDirection] = useState(0);
 
   useEffect( () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => setCoords(position.coords));
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCoords(position.coords);
+        setHeading(position.coords.heading);
+        setSchutzDirection(toDegrees(bearing(Schutzi, position.coords)));
+      });
     }
   }, []);
 
-  // the marker should mark the Schützi toDegrees(bearing(coords, Schutzi)) from geolocations
   return (
     <div className="App">
-      <ReactCompass direction={0} marker={coords === undefined || coords.heading === null ? rangeVal : coords.heading}/>
-      <input type="range" max="360" min="0"
-          onChange={(e) => setRangeVal(parseInt(e.target.value, 10))} onInput={(e) => setRangeVal(parseInt(e.target.value, 10))}/>
-      { coords !== undefined ? <div>
-                        <div> Latitude: {coords.latitude}</div>
-                        <div> Longitude: {coords.longitude}</div>
-                      <div>  Accuracy: {coords.accuracy}</div>
-                      <div>  Heading: {toDegrees(bearing(Schutzi, coords))}</div>
-
-                                 </div> : <div> Please enable geolocation!</div>
+      { coords !== undefined ?
+      <div>
+        <ReactCompass direction={heading} marker={(-heading + schutzDirection) % 360} />
+        <input type="range" max="360" min="0" value={schutzDirection}
+          onChange={(e) => setSchutzDirection(parseInt(e.target.value, 10))}
+          onInput={(e) => setSchutzDirection(parseInt(e.target.value, 10))}/>
+        <input type="range" max="360" min="0" value={heading}
+          onChange={(e) => setHeading(parseInt(e.target.value, 10))}
+          onInput={(e) => setHeading(parseInt(e.target.value, 10))}/>
+        <div>
+          <div> Latitude: {coords.latitude}</div>
+          <div> Longitude: {coords.longitude}</div>
+          <div> Accuracy: {coords.accuracy}</div>
+          <div> SchütziDirection: {schutzDirection}</div>
+        </div>
+      </div> : <div> Please enable geolocation!</div>
       }
     </div>
   );
